6e1c641770bf0e4ded2b0b24237732f0db0132fc
Scott Weiss <sdw35@cornell.edu>
2017-06-16 15:09:17 -0400

    refactor test

--- e2e_test.go
+++ e2e_test.go
@@ -18,88 +18,82 @@ import (
 )
 
 var (
-	storagePath, listenAddr string
+	storagePath   string
+	listenAddr    = ":9099"
+	retention     = 24 * time.Hour
+	gcInterval    = 10 * time.Minute
+	pushInterval  = 5 * time.Second
+	serverStarted bool
 )
 
-func TestAppendGet(t *testing.T) {
-	dir, err := ioutil.TempDir(".", "e2e_test_")
-	if err != nil {
-		t.Fatal("unexpected err: %v", err)
-	}
-	defer os.RemoveAll(dir)
-	var tests = []struct {
-		description                         string
-		retention, gcInterval, pushInterval time.Duration
-	}{
-		{
-			description:  "default values",
-			retention:    24 * time.Hour,
-			gcInterval:   10 * time.Minute,
-			pushInterval: 5 * time.Second,
-		},
-	}
-	storagePath = filepath.Join(dir, "messages.db")
-	listenAddr = ":9099"
-
-	for _, test := range tests {
-		t.Logf("running text with context: %#v", test)
+func initServer(dir string, t *testing.T) {
+	if !serverStarted {
 		go func() {
+			storagePath = filepath.Join(dir, "messages.db")
 			t.Logf("starting server")
-			if err := runService(storagePath, listenAddr, test.retention, test.gcInterval, test.pushInterval); err != nil {
+			serverStarted = true
+			if err := runService(storagePath, listenAddr, retention, gcInterval, pushInterval); err != nil {
 				t.Fatalf("server encountered unexpected error: %v", err)
 			}
 		}()
-
 		if err := waitServerStart(); err != nil {
 			t.Fatalf("server encountered unexpected error: %v", err)
 		}
+	}
+}
 
-		genID, err := getGenerationID()
-		if err != nil {
-			t.Fatalf("failed to retrieve generation ID from server: %v", err)
-		}
+func TestE2EAppendGet(t *testing.T) {
+	dir, err := ioutil.TempDir(".", "e2e_test_")
+	if err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+	defer os.RemoveAll(dir)
+	initServer(dir, t)
+	genID, err := getGenerationID()
+	if err != nil {
+		t.Fatalf("failed to retrieve generation ID from server: %v", err)
+	}
 
-		topics := []string{
-			"topic0",
-			"topic1",
-			"topic2",
-			"topic3",
-		}
-		items := []map[string]interface{}{
-			{"A": "Hi", "B": 0.0},
-			{"A": "Hello", "B": 1.0},
-			{"A": "Bonjour", "B": 2.0},
-			{"A": "Hola", "B": 3.0},
-			{"A": "Shalon", "B": 4.0},
-		}
-		for _, topic := range topics {
-			idx := 1
-			for _, item := range items {
-				if err := doAppend(item, topic); err != nil {
-					t.Fatalf("failed to perform append: %v", err)
-				}
-				fromIndex := fmt.Sprintf("%v", idx)
-				//return 1 object at a time
-				msgs, err := doGet(topic, genID, fromIndex)
-				if err != nil {
-					t.Fatalf("failed to get messages from server: %v", err)
-				}
-				if msgs.GenerationID != genID {
-					t.Fatalf("server did not return expected generation ID: %s != %s", msgs.GenerationID, genID)
-				}
-				if len(msgs.Messages) != 1 {
-					t.Fatalf("server did not return expected number of objects: %v != 1", len(msgs.Messages))
-				}
-				msg := msgs.Messages[0]
-				retItem, ok := msg.Data.(map[string]interface{})
-				if !ok {
-					t.Fatalf("type of message did not match expected: %v != map[string]interface{}", reflect.TypeOf(retItem))
-				}
-				if !reflect.DeepEqual(retItem, item) {
-					t.Fatalf("returned item did not match expected: %v != %v", retItem, item)
-				}
-				idx++
+	topics := []string{
+		"topic0",
+		"topic1",
+		"topic2",
+		"topic3",
+	}
+	items := []map[string]interface{}{
+		{"A": "Hi", "B": 0.0},
+		{"A": "Hello", "B": 1.0},
+		{"A": "Bonjour", "B": 2.0},
+		{"A": "Hola", "B": 3.0},
+		{"A": "Shalon", "B": 4.0},
+	}
+	for _, topic := range topics {
+		idx := 1
+		for _, item := range items {
+			if err := doAppend(item, topic); err != nil {
+				t.Fatalf("failed to perform append: %v", err)
 			}
+			fromIndex := fmt.Sprintf("%v", idx)
+			//return 1 object at a time
+			msgs, err := doGet(topic, genID, fromIndex)
+			if err != nil {
+				t.Fatalf("failed to get messages from server: %v", err)
+			}
+			if msgs.GenerationID != genID {
+				t.Fatalf("server did not return expected generation ID: %s != %s", msgs.GenerationID, genID)
+			}
+			if len(msgs.Messages) != 1 {
+				t.Fatalf("server did not return expected number of objects: %v != 1", len(msgs.Messages))
+			}
+			msg := msgs.Messages[0]
+			retItem, ok := msg.Data.(map[string]interface{})
+			if !ok {
+				t.Fatalf("type of message did not match expected: %v != map[string]interface{}", reflect.TypeOf(retItem))
+			}
+			if !reflect.DeepEqual(retItem, item) {
+				t.Fatalf("returned item did not match expected: %v != %v", retItem, item)
+			}
+			idx++
 		}
 	}
 }
